a2f7b0de86029f1613cc6233c1ddaefd03c06c6e
rztprog <rztprog@outlook.fr>
2023-01-23 16:44:27 +0100

    Add validation to contact controller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,15 @@
 class Contact < MailForm::Base
-  attribute :name
-  attribute :surname
-  attribute :email
-  attribute :message
+  attribute :name,      validate: true
+  attribute :surname,   validate: true
+  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   validate: true
 
   def headers
-    { to: "l.broion@ħotmail.fr" }
+    { 
+      to: "l.broion@ħotmail.fr", # change this to be the email you want sent to
+      subject: "Solenne Carreras Contact",
+      from: "admin@yourdomain.com",  # change this to be the email it is coming from
+      reply_to: %("#{name}" <#{email}>) 
+    }
   end
 end
