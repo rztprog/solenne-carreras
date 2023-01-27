4859ef5e8b37616a73b387529bae413dcb1729d7
rztprog <rztprog@outlook.fr>
2023-01-27 10:08:28 +0100

    Add new service to storage.yml, add service to photographie model, change custom folder link to img in show

--- app/models/photographie.rb
+++ app/models/photographie.rb
@@ -1,5 +1,5 @@
 class Photographie < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :description, presence: true
-  has_one_attached :photo
+  has_one_attached :photo, service: :cloudinary_photographies
 end
