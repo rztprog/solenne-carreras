e821ce7d70aa34a19427732fa7de38320a2b28ae
rztprog <rztprog@outlook.fr>
2023-02-02 10:18:28 +0100

    Add hidden class, move data-controller to layout, add target and body overflow hidden when click on menu burger, change navbar z-index from 1 to 2, change font-size with media querie and add regular font-family

--- app/javascript/controllers/menu_burger_controller.js
+++ app/javascript/controllers/menu_burger_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "open", "close"];
+  static targets = ["menu", "open", "close", "body"];
 
   // connect() {
   //   console.log("Menu burger stimulus controller connected !")
@@ -11,5 +11,6 @@ export default class extends Controller {
     this.menuTarget.classList.toggle("active");
     this.openTarget.classList.toggle("active");
     this.closeTarget.classList.toggle("active");
+    this.bodyTarget.classList.toggle("hidden");
   }
 }
