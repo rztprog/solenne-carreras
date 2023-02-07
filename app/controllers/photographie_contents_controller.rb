66283be7f3972fb33d61d723b509781d2980827b
rztprog <rztprog@outlook.fr>
2023-02-07 09:52:41 +0100

    Add controller method

--- app/controllers/photographie_contents_controller.rb
+++ app/controllers/photographie_contents_controller.rb
@@ -1,2 +1,34 @@
 class PhotographieContentsController < ApplicationController
+  before_action :set_photographie, only: [:new, :create, :destroy]
+
+  def new
+    @photographieContent = PhotographieContent.new
+  end
+
+  def create
+    @photographieContent = PhotographieContent.new(photographieContent_params)
+    @photographieContent.photographie = @photographie
+
+    if @photographieContent.save
+      redirect_to photographie_path(@photographie)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @photographieContent = PhotographieContent.find(params[:id])
+    @photographieContent.destroy
+    redirect_to photographie_path(@photographie)
+  end
+
+  private
+
+  def photographieContent_params
+    params.require(:photographieContent).permit()
+  end
+
+  def set_photographie
+    @photographie = Photographie.find(params[:photography_id])
+  end
 end
