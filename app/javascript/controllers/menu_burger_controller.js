7aba7171b9878e5f00ef54dffcd25ef7032d1ee6
rztprog <rztprog@outlook.fr>
2023-02-11 17:25:45 +0100

    Change icon style open/close with click effect

--- app/javascript/controllers/menu_burger_controller.js
+++ app/javascript/controllers/menu_burger_controller.js
@@ -1,16 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "open", "close", "body"];
+  static targets = ["desktopList", "burger", "body", "span1", "span2", "span3"];
 
   // connect() {
   //   console.log("Menu burger stimulus controller connected !")
   // }
 
   action() {
-    this.menuTarget.classList.toggle("active");
-    this.openTarget.classList.toggle("active");
-    this.closeTarget.classList.toggle("active");
+    this.desktopListTarget.classList.toggle("active");
+    this.burgerTarget.classList.toggle("active");
     this.bodyTarget.classList.toggle("hidden");
+
+    this.span1Target.classList.toggle("span1");
+    this.span2Target.classList.toggle("span2");
+    this.span3Target.classList.toggle("span3");
   }
 }
