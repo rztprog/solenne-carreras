3ab5f9e46f52181822a5d0383f7c86fb8c567dce
rztprog <rztprog@outlook.fr>
2023-02-15 11:21:56 +0100

    Add validation for photo type in content and in photographie

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,20 @@
 class Content < ApplicationRecord
-  # validates :photos, presence: true
+  validates :photos, presence: true
+  validate :validate_photos_filetype
 
   belongs_to :photographie
 
   has_many_attached :photos, service: :cloudinary_photographie_contents
+
+  private
+
+  def validate_photos_filetype
+    return unless photos.attached?
+  
+    photos.each do |photo|
+      unless photo.image?
+        errors.add(:photos, "must be an image")
+      end
+    end
+  end
 end
--- app/models/photographie.rb
+++ app/models/photographie.rb
@@ -2,8 +2,19 @@ class Photographie < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :description, presence: true
   validates :photo, presence: true
+  validate :validate_photo_filetype
 
   has_many :contents, dependent: :destroy
 
   has_one_attached :photo, service: :cloudinary_photographies
+
+  private
+
+  def validate_photo_filetype
+    return unless photo.attached?
+
+    unless photo.image?
+      errors.add(:photo, "must be an image")
+    end
+  end
 end
