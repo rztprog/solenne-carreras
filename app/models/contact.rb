49ba05bb7113725e1071f652b4bacff84f9af9f8
rztprog <rztprog@outlook.fr>
2023-02-22 15:29:47 +0100

    Change email text and style

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,13 +4,13 @@ class Contact < MailForm::Base
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   validate: true
 
-  def headers
-    { 
-      to: "spitemplar@gmail.com", # change this to be the email you want sent to
-      subject: "Solenne-Carreras - Nouveau message de #{surname}",
-      # Changer le mail dans l'environement de development
-      from: %("#{name} #{surname}" <#{email}),  # change this to be the email it is coming from
-      reply_to: %("#{name} #{surname}" <#{email}>) 
-    }
-  end
+  # def headers
+  #   { 
+  #     to: "spitemplar@gmail.com", # change this to be the email you want sent to
+  #     subject: "Solenne-Carreras - Nouveau message de #{surname}",
+  #     # Changer le mail dans l'environement de development
+  #     from: %("#{name} #{surname}" <#{email}),  # change this to be the email it is coming from
+  #     reply_to: %("#{name} #{surname}" <#{email}>) 
+  #   }
+  # end
 end
