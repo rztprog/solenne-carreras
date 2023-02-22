1080a2555725a84038c28f7d54b27e973200e9d3
rztprog <rztprog@outlook.fr>
2023-02-22 16:38:38 +0100

    Custom mail preview, change subject by message.subject, add default from

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,6 @@
 class ContactMailer < ApplicationMailer
+  default from: 'noreply@solennecarreras.com'
+
   def contact(data)
     @data = data
     mail(to: 'spitemplar@gmail.com', subject: "Nouveau message de #{@data.surname + ' ' + @data.name}")
