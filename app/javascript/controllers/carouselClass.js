9e4485e0cdd69aa951657ff75bd0cbfede284043
rztprog <rztprog@outlook.fr>
2023-03-07 17:25:02 +0100

    Add new vanilla js carrousel

---
+++ app/javascript/controllers/carouselClass.js
@@ -0,0 +1,116 @@
+export class Carousel {
+  constructor (element, options = {}){
+    this.element = element;
+    this.options = Object.assign({}, {
+      slidesToScroll: 1,
+      slidesVisible: 1
+    }, options);
+    let children = [].slice.call(element.children);
+
+    this.isMobile = false;
+    this.isTablet = false;
+    this.currentItem = 0;
+
+    this.root = this.createDivWithClass("carousel");
+    this.root.setAttribute("tabindex", "0");
+    this.container = this.createDivWithClass("carousel-container");
+
+
+    this.root.appendChild(this.container);
+    this.element.appendChild(this.root);
+
+
+    this.items = children.map((child) => {
+      let item = this.createDivWithClass("carousel-item");
+      item.appendChild(child);
+      this.container.appendChild(item);
+      return item;
+    })
+
+    this.setStyle();
+    this.createNavigation();
+
+    this.onWindowResize();
+    window.addEventListener("resize", this.onWindowResize.bind(this));
+
+    this.root.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowRight") {
+        this.nextPic();
+      } else if (e.key === "ArrowLeft") {
+        this.prevPic();
+      }
+    })
+  }
+
+  onWindowResize () {
+    let mobile = window.innerWidth < 1000;
+    let tablet = window.innerWidth < 1600 && window.innerWidth > 1000;
+
+    if (tablet !== this.isTablet) {
+      this.isTablet = tablet;
+      this.setStyle();
+    }
+
+    if (mobile !== this.isMobile) {
+      this.isMobile = mobile;
+      this.setStyle();
+    }
+  }
+
+  createDivWithClass (className) {
+    let div = document.createElement('div');
+    div.setAttribute('class', className);
+    return div;
+  }
+
+  setStyle () {
+    let ratio = this.items.length / this.slidesVisible;
+    this.container.style.width = (ratio * 100) + "%";
+
+    this.items.forEach(item => {
+      item.style.width = (100 / this.slidesVisible / ratio) + "%";
+    });
+  }
+
+  createNavigation() {
+    let prevButton = this.createDivWithClass("carousel-prev");
+    let nextButton = this.createDivWithClass("carousel-next");
+
+    this.root.appendChild(prevButton);
+    this.root.appendChild(nextButton);
+
+    prevButton.addEventListener('click', this.prevPic.bind(this));
+    nextButton.addEventListener('click', this.nextPic.bind(this));
+  }
+
+  nextPic(){
+    this.gotoItem(this.currentItem + this.slidesToScroll);
+  }
+
+  prevPic(){
+    this.gotoItem(this.currentItem - this.slidesToScroll, true);
+  }
+
+  gotoItem (index, previewPress){
+    if (index < 0) {
+      index = this.items.length - this.slidesVisible;
+    } else if (index >= this.items.length || this.items[this.currentItem + this.slidesVisible] === undefined)  {
+      previewPress ? index : index = 0;
+    }
+    let translateX = index * -100 / this.items.length;
+    this.container.style.transform = `translate3D(${translateX}%, 0, 0)`;
+    this.currentItem = index;
+  }
+
+  get slidesToScroll () {
+    return this.isMobile || this.isTablet ? 1 : this.options.slidesToScroll;
+  }
+
+  get slidesVisible () {
+    if (this.isTablet) {
+      return 2;
+    } else {
+      return this.isMobile ? 1 : this.options.slidesVisible;
+    }
+  }
+}
--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,43 +1,14 @@
-import { Application } from '@hotwired/stimulus'
-import Carousel from 'stimulus-carousel'
+import { Controller } from "@hotwired/stimulus"
+import { Carousel } from "./carouselClass"
 
-const application = Application.start()
-application.register('carousel', Carousel)
+export default class extends Controller {
+  static targets = ["carousel1"];
 
-let slides = 3; 
-
-export default class extends Carousel {
   connect() {
-    super.connect()
-    // console.log('Do what you want here.')
-
-    // The swiper instance.
-    // this.swiper
-
-    // Default options for every carousels.
-    this.defaultOptions
-  }
-
-  // You can set default options in this getter.
-  get defaultOptions() {
-
-    return {
-      // Your default options here
-        slidesPerView: slides,
-        slidesPerGroup: 1,
-        loop: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }
-    }
+    new Carousel(this.carousel1Target, {
+      slidesToScroll: 1,
+      slidesVisible: 3,
+      loop: false
+    });
   }
 }
-
-window.addEventListener("resize", function () {
-  if (window.innerWidth < 730) {
-    slides = 1;
-  } else {
-    slides = 3;
-  }
-});
